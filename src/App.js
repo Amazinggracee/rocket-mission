53683afb97e9a06ed1fa4f0737dd8aec4b4b9993
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-05-16 07:50:22 +0100

    Add some files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <h1>Hello World!</h1>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" />
       </Routes>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from './store/configStore';
+
+import './index.css';
 import App from './App';
-import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
         <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
 );
